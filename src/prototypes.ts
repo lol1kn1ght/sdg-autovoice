001b00363c6734243757400246fb224ad0e8e9bb
lol1kn1ght <loliknight@easygaming.su>
2022-09-17 17:13:43 +0300

    dev

--- src/prototypes.ts
+++ src/prototypes.ts
@@ -6,7 +6,6 @@ function curr_time() {
 }
 
 const log = console.log;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 console.log = function (...args: any[]) {
   process.stdout.write(curr_time().white);
   log(...args);
@@ -14,13 +13,11 @@ console.log = function (...args: any[]) {
 
 //  process.stdout.write(curr_time().white);
 //  log(...args);
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 console.warn = function (...args: any[]) {
   process.stdout.write(curr_time().yellow);
   log(...args.map((arg) => (typeof arg === 'string' ? arg.yellow : arg)));
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 console.error = function (...args: any[]) {
   process.stdout.write(curr_time().red);
   log(...args.map((arg) => (typeof arg === 'string' ? arg.red : arg)));
